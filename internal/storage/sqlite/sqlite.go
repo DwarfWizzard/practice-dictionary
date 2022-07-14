16292478e94f83d6308f8c90aeb691e1a27494be
DwarfWizzard <daerdq@mail.ru>
2022-07-14 04:07:04 +0300

    a

--- internal/storage/sqlite/sqlite.go
+++ internal/storage/sqlite/sqlite.go
@@ -1,11 +1,18 @@
 package sqlite
 
 import (
+	"errors"
+	"os"
+
 	"github.com/jmoiron/sqlx"
 	_ "github.com/mattn/go-sqlite3"
 )
 
 func NewSQLiteConn(dbPath string) (*sqlx.DB, error) {
+	if _, err := os.Stat(dbPath); err != nil {
+		return nil, errors.New("the database on the route `"+dbPath+"` does not exist")
+	}
+
 	conn, err := sqlx.Connect("sqlite3", dbPath)
 	if err != nil {
 		return nil, err
